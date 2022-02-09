3c53357a3913668591fe10f913af24d21a723747
janderson390 <janderson390@student.cvtc.edu>
2022-02-09 15:02:34 -0600

    Update character.js

--- js/character.js
+++ js/character.js
@@ -1,34 +1,7 @@
-class Character {
-  location = {x: 0, y: 0};
-  inventory = {};
+  let fighter = {atk: 12, dex: 10, con: 10};
+  let ranger = {atk: 10, dex: 12, con: 10};
+  let brute = {atk: 10, dex: 10, con: 12};
 
-  // Constructor
-  constructor(name, stats) {
-    this.name = name;
-    this.stats = stats; // Stats will be a dictionary of values containing str, dex, con
-  }
-
-  // Getters
-  get location() {
-    return this.location;
-  }
-
-  get inventory() {
-    return this.inventory;
-  }
-
-  get name() {
-    return this.name;
-  }
-
-  get stats() {
-    return this.stats;
-  }
-
-};
-
-// Character test
-// let character = new Character("Samuel", {str: 10, dex: 10, con: 10});
-// character.location = {x: 1, y: 1};
-// character.inventory = {weapon: "sword", potion: "health"};
-// console.log(character);
+function createCharacter(name, className, ) {
+  var character = {name: "", stats: className, location: {x: 0, y: 0}};
+}
