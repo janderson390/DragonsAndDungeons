0ed9d33eccba03a06666154a44cf640b75c53b11
janderson390 <janderson390@student.cvtc.edu>
2022-02-10 16:18:16 -0600

    deleted the unused character class

--- js/character.js
+++
@@ -1,8 +0,0 @@
-  let fighter = {atk: 12, dex: 10, con: 10};
-  let ranger = {atk: 10, dex: 12, con: 10};
-  let brute = {atk: 10, dex: 10, con: 12};
-
-function createCharacter(name, className) {
-  var character = {name: name, stats: className, location: {x: 0, y: 0}};
-  return character;
-}
